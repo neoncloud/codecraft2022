d0df20bcc69bc0bbca3a6821b9c6649120f76146
neoncloud <1840652@brunel.ac.uk>
2023-03-11 13:36:55 +0800

    搞定parser

--- frame.py
+++ frame.py
@@ -11,6 +11,15 @@ class Workbench:
             reversed(bin(int(material_state))[2:])) if bit == "1"]
         self.product_state = product_state
 
+    def update(self, remaining_time:int=None, material_state:int=None, product_state:bool=None):
+        if remaining_time is not None:
+            self.remaining_time = remaining_time
+        if material_state is not None:
+            self.material_state = [j for j, bit in enumerate(
+                reversed(bin(int(material_state))[2:])) if bit == "1"]
+        if product_state is not None:
+            self.product_state = product_state
+
 
 class Robot:
     def __init__(self, workbench_id: int, carrying_item: int, time_value_coeff: float, collision_value_coeff: float,
@@ -24,20 +33,46 @@ class Robot:
         self.heading = heading
         self.coord = np.array([x, y])
 
+    def update(self, workbench_id:int=None, carrying_item:int=None, time_value_coeff:float=None, collision_value_coeff:float=None,
+               angular_velocity:float=None, linear_velocity_x: float=None, linear_velocity_y: float=None, heading:float=None, x:float=None, y:float=None):
+        if workbench_id is not None:
+            self.workbench_id = workbench_id
+        if carrying_item is not None:
+            self.carrying_item = carrying_item
+        if time_value_coeff is not None:
+            self.time_value_coeff = time_value_coeff
+        if collision_value_coeff is not None:
+            self.collision_value_coeff = collision_value_coeff
+        if angular_velocity is not None:
+            self.angular_velocity = angular_velocity
+        if linear_velocity_x is not None and linear_velocity_y is not None:
+            self.linear_velocity = np.array([linear_velocity_x, linear_velocity_y])
+        if heading is not None:
+            self.heading = heading
+        if x is not None and y is not None:
+            self.coord = np.array([x, y])
 
 class Map:
-    def __init__(self, frame_num: int, money: int, workbenches: List[Workbench]):
+    def __init__(self, frame_num: int, money: int, workbenches: List[Workbench], robots:List[Robot]):
         self.frame_num = frame_num
         self.money = money
         self.workbenches = workbenches
-
-
-class Agent:
-    def __init__(self, robots: List[Robot]):
         self.robots = robots
 
+    def update(self, input_string:str):
+        lines = input_string.strip().split('\n')
+        self.frame_num, self.money = map(int, lines[0].split())
+        workbench_count = int(lines[1])
+        # for i in range(2, 2 + workbench_count):
+        for i, w in enumerate(self.workbenches, 2):
+            workbench_data = list(map(float, lines[i].split()))
+            w.update(int(workbench_data[3]), int(workbench_data[4]), bool(workbench_data[5]))
+        for i, r in enumerate(self.robots, 2 + workbench_count):
+            robot_data = list(map(float, lines[i].split()))
+            r.update(int(robot_data[0]), int(robot_data[1]), robot_data[2], robot_data[3], robot_data[4], robot_data[5], robot_data[6], robot_data[7], robot_data[8], robot_data[9])
 
-def parse_input(input_string: str) -> Tuple[Map, Agent]:
+
+def parse_input(input_string: str) -> Map:
     lines = input_string.strip().split('\n')
     frame_num, money = map(int, lines[0].split())
     workbench_count = int(lines[1])
@@ -53,6 +88,5 @@ def parse_input(input_string: str) -> Tuple[Map, Agent]:
         robot = Robot(int(robot_data[0]), int(robot_data[1]), robot_data[2], robot_data[3],
                       robot_data[4], robot_data[5], robot_data[6], robot_data[7], robot_data[8], robot_data[9])
         robots.append(robot)
-    map_obj = Map(frame_num, money, workbenches)
-    agent = Agent(robots)
-    return map_obj, agent
+    map_obj = Map(frame_num, money, workbenches, robots)
+    return map_obj
--- task.py
+++ task.py
@@ -1,7 +1,17 @@
-class Task:
-    def __init__(self, start, end, cost, profit, dep=None) -> None:
-        self.start = start
-        self.end = end
-        self.cost = cost
-        self.profit = profit
-        self.dep = dep
+import numpy as np
+from frame import Map
+
+class Scheduler:
+    def __init__(self, adj_mat:np.array, workbench:dict, map_obj:Map) -> None:
+        self.adj_mat = adj_mat
+        self.workbench = workbench
+        self.map = map_obj
+
+    def get_nearest_workbench(self, id:int, robot_pos:int):
+        workbench_list = self.workbench[id]
+        workbench_list = [i for i in workbench_list if self.map.workbenches[i].product_state]
+        dist = self.adj_mat[workbench_list, robot_pos]
+        return workbench_list[np.argmin(dist)]
+    
+    def update(self):
+        pass
