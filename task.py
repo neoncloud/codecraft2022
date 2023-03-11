c0dda8d2a07c3b852aa34b66164ef73ade01dafb
neoncloud <1840652@brunel.ac.uk>
2023-03-11 15:30:05 +0800

    实现了一个最简单的调度器

--- task.py
+++ task.py
@@ -1,17 +1,34 @@
 import numpy as np
 from frame import Map
+from queue import Queue
 
 class Scheduler:
-    def __init__(self, adj_mat:np.array, workbench:dict, map_obj:Map) -> None:
-        self.adj_mat = adj_mat
-        self.workbench = workbench
+    def __init__(self, map_obj:Map) -> None:
+        self.tasks_queue = Queue()
         self.map = map_obj
+        self.rule = {
+            1:(4,5),
+            2:(4,6),
+            3:(5,6),
+            4:(7,9),
+            5:(7,9),
+            6:(7,9),
+            7:(8,9),
+            8:(),
+            9:()
+        }
+        self.wb_type_to_id = {i:[w for w,_ in enumerate(self.map.workbenches) if _.type_id==i] for i in range(1,10)}
+        self.wb_id_to_type = {i:w.type_id for i,w in enumerate(self.map.workbenches)}
+        self.src_to_tgt = {i:[w for t in self.rule[_.type_id] for w in self.wb_type_to_id[t]] for i,_ in enumerate(self.map.workbenches)}
 
-    def get_nearest_workbench(self, id:int, robot_pos:int):
-        workbench_list = self.workbench[id]
-        workbench_list = [i for i in workbench_list if self.map.workbenches[i].product_state]
-        dist = self.adj_mat[workbench_list, robot_pos]
-        return workbench_list[np.argmin(dist)]
-    
-    def update(self):
-        pass
+    def update(self, map_obj=None):
+        if map_obj is not None:
+            self.map = map_obj
+        source = list(filter(lambda x: self.map.workbenches[x].product_state, range(len(self.map.workbenches))))
+        for s in source:
+            target_candidate = self.src_to_tgt[s]
+            target_candidate = list(filter(lambda w: self.wb_id_to_type[s] not in self.map.workbenches[w].material_state, target_candidate))
+            if len(target_candidate)>0:
+                dist = self.map.adj_mat[s,np.array(target_candidate)]
+                self.tasks_queue.put((s,target_candidate[np.argmin(dist)]))
+
